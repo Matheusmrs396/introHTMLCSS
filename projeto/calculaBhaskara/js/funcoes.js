27d67e1ba07388cf93e29f7123ea16901ecde8d2
MatheusSilva <matheusmrs396@gmail.com>
2022-04-27 09:02:10 -0300

    Finalizando a calculadora

--- projeto/calculaBhaskara/js/funcoes.js
+++ projeto/calculaBhaskara/js/funcoes.js
@@ -1,11 +1,16 @@
-export {calculaDelta, calculaBhaskara}
+export { calculaDelta, calculaBhaskara };
 
-function calculaDelta(a,b,c){
-    return (b*b) - 4*a*c; 
+function calculaDelta(a, b, c) {
+  return b * b - 4 * a * c;
 }
 
-function calculaBhaskara(a,b,delta){
-    let x = (-b + Math.sqrt(delta))/ (2*a);
-    let x2 = (-b - Math.sqrt(delta))/ (2*a);
-    return {x,x2};
+function calculaBhaskara(a, b, delta) {
+  if (delta == 0) {
+    let x = -b / (2 * a);
+    return { x };
+  } else {
+    let x = (-b + Math.sqrt(delta)) / (2 * a);
+    let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+    return { x, x2 };
+  }
 }
