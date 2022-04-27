235db3ad3fcc13aa652efe47ba0ad6d8ac1996e3
MatheusSilva <matheusmrs396@gmail.com>
2022-04-27 10:34:37 -0300

    Alterando sistema de pastas da calculadora

--- projeto/calculaBhaskara/js/index.js
+++ projeto/calculaBhaskara/js/index.js
@@ -1,32 +1,21 @@
-import { calculaDelta, calculaBhaskara } from "./funcoes.js";
+import {calculaDelta, calculaBhaskara} from './funcoes.js';
 
-function chamaFuncao() {
-  let a = document.getElementById("a").value;
-  let b = document.getElementById("b").value;
-  let c = document.getElementById("c").value;
-  let delta = calculaDelta(a, b, c);
-  if (delta < 0) {
-    document.getElementById("result").style.display = "none";
-    document.getElementById("semRaiz").style.display = "block";
-  } else {
-    let bhaskara = calculaBhaskara(a, b, delta);
-    console.log(bhaskara.x);
+function chamaFuncao(){
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+    let c = document.getElementById('c').value;
+    let delta = calculaDelta(a,b,c);
+    console.log(delta);
+    let bhaskara = calculaBhaskara(a,b,delta);
+    console.log(bhaskara);
     document.getElementById("delta").value = delta;
     document.getElementById("x").value = bhaskara.x;
-    if (bhaskara.x2) {
-      document.getElementById("x2").value = bhaskara.x2;
-    } else {
-      document.getElementById("x2").style.display = "none";
-      document.getElementById("spanX2").style.display = "none";
-    }
-    document.getElementById("semRaiz").style.display = "none";
-    document.getElementById("result").style.display = "block";
-  }
+    document.getElementById("x2").value = bhaskara.x2;
+    document.getElementById("result").style.display ="block";
 }
 
-window.onload = function () {
-  document.getElementById("result").style.display = "none";
-  document.getElementById("semRaiz").style.display = "none";
-};
+window.onload = function() {
+        document.getElementById("result").style.display ="none";
+}
 
-document.getElementById("btn").addEventListener("click", chamaFuncao);
+document.getElementById("btn").addEventListener('click', chamaFuncao);
